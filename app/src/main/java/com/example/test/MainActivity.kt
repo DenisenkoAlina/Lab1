826a397aceb8a7b65453b199e6af56848eb71e2b
Adena-Den <kaa2000star@gmail.com>
2020-03-04 00:31:27 +0200

    My Calculator

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,18 +10,16 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
-    var isLastNumeric: Boolean = false
-    var isLastPointer: Boolean = false
-    var stateError: Boolean = false
-    var isOpenBkt: Boolean = false
-    var isCloseBkt: Boolean = false
+    var isLastNumeric: Boolean = false //Checks if the last character is a number
+    var isLastPointer: Boolean = false //Checks if the last character is a pointer
+    var stateError: Boolean = false //Error catching variable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun onNumber(view: View) {
+    fun onNumber(view: View) { //Function of number input
         if (stateError) {
             textView.text = (view as Button).text
             stateError = false
@@ -28,13 +27,11 @@ class MainActivity : AppCompatActivity() {
             textView.append((view as Button).text)
         }
         isLastNumeric = true
-        isCloseBkt = false
-        isOpenBkt = false
         if (isLastNumeric && !textView.text.endsWith("0"))
-            answer.text = arifmetic().toString()
+            answer.text = arithmetic().toString()
     }
 
-    fun onDecimal(view: View) {
+    fun onDecimal(view: View) { //Function of pointer input
         if (!isLastPointer && !stateError) {
             if (!isLastNumeric) {
                 textView.append("0")
@@ -42,39 +39,34 @@ class MainActivity : AppCompatActivity() {
             textView.append((view as Button).text)
             isLastPointer = true
             isLastNumeric = false
-            isCloseBkt = false
-            isOpenBkt = false
         }
     }
 
-    fun onOperator(view: View) {
+    fun onOperator(view: View) { //Function of operator input
         if (isLastNumeric && !stateError) {
             textView.append((view as Button).text)
             isLastPointer = false
             isLastNumeric = false
-            isCloseBkt = false
-            isOpenBkt = false
         }
     }
 
-    fun onClean(view: View) {
+    fun onClean(view: View) { //Function of clear
         textView.text = ""
         answer.text = ""
         isLastPointer = false
         isLastNumeric = false
-        isCloseBkt = false
-        isOpenBkt = false
         stateError = false
     }
 
-    fun onBackspace(view: View) {
+    fun onBackspace(view: View) { //Function to delete the last character of a string
         textView.text = textView.text.toString().dropLast(1)
     }
 
-    fun onResult(view: View) {
+    @SuppressLint("SetTextI18n")
+    fun onResult(view: View) { //Output function
         if (isLastNumeric && !stateError) {
             try {
-                textView.text = arifmetic().toString()
+                textView.text = arithmetic().toString()
                 answer.text = ""
                 isLastPointer = true
             } catch (ex: ArithmeticException) {
@@ -85,9 +77,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun arifmetic():Double {
+    private fun arithmetic():Double { //Math function
         val txt = textView.text.toString()
-        val expression = ExpressionBuilder(txt).build()
-        return expression.evaluate()
+        val expression = ExpressionBuilder(txt).build() //Library to convert text to equation
+        return expression.evaluate() //Answer
     }
 }
